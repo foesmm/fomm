abb07418ca1e250850d9e042d2014ad149597a3c
timeslip <timeslip@727d929f-1e04-4230-bf68-c6d161abaee7>
2008-12-23 15:57:14 +0000

    Remove renamed files

--- fomm.Scripting/Script.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace fomm.Scripting {
-    public abstract class Script : fomm.PackageManager.BaseScript {
-        public new string[] GetFileList() { return base.GetFileList(); }
-    }
-}
