696a34e12045c6faddc6965c60d650fef02c8f3a
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-02-17 00:44:03 +0000

    Changed the file manager to use our custom transactions instead of the .net transactions.

--- ChinhDo.Transactions.FileManager/TxEnlistment.cs
+++ ChinhDo.Transactions.FileManager/TxEnlistment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Transactions;
+using fomm.Transactions;
 
 namespace ChinhDo.Transactions
 {
--- ChinhDo.Transactions.FileManager/TxFileManager.cs
+++ ChinhDo.Transactions.FileManager/TxFileManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Transactions;
+using fomm.Transactions;
 
 namespace ChinhDo.Transactions
 {
