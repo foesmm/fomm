b2b69b7833204ca502f696d633645ca811b0b41a
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-04-22 20:19:16 +0000

    Changed how the form autosizes. Hopefully this will the fix occasionnal problem users are having with the form being too small for the contents.

--- flmm/PackageManager/Overwriteform.Designer.cs
+++ flmm/PackageManager/Overwriteform.Designer.cs
@@ -30,12 +30,16 @@ namespace Fomm.PackageManager {
 			this.bNoToFolder = new System.Windows.Forms.Button();
 			this.bNo = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// bYesToAll
 			// 
 			this.bYesToAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bYesToAll.Location = new System.Drawing.Point(14, 73);
+			this.bYesToAll.Location = new System.Drawing.Point(14, 4);
 			this.bYesToAll.Name = "bYesToAll";
 			this.bYesToAll.Size = new System.Drawing.Size(75, 23);
 			this.bYesToAll.TabIndex = 1;
@@ -46,7 +50,7 @@ namespace Fomm.PackageManager {
 			// bYesToFolder
 			// 
 			this.bYesToFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bYesToFolder.Location = new System.Drawing.Point(95, 73);
+			this.bYesToFolder.Location = new System.Drawing.Point(95, 4);
 			this.bYesToFolder.Name = "bYesToFolder";
 			this.bYesToFolder.Size = new System.Drawing.Size(75, 23);
 			this.bYesToFolder.TabIndex = 2;
@@ -57,7 +61,7 @@ namespace Fomm.PackageManager {
 			// bYes
 			// 
 			this.bYes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bYes.Location = new System.Drawing.Point(176, 73);
+			this.bYes.Location = new System.Drawing.Point(176, 4);
 			this.bYes.Name = "bYes";
 			this.bYes.Size = new System.Drawing.Size(75, 23);
 			this.bYes.TabIndex = 3;
@@ -68,7 +72,7 @@ namespace Fomm.PackageManager {
 			// bNoToAll
 			// 
 			this.bNoToAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bNoToAll.Location = new System.Drawing.Point(257, 73);
+			this.bNoToAll.Location = new System.Drawing.Point(257, 4);
 			this.bNoToAll.Name = "bNoToAll";
 			this.bNoToAll.Size = new System.Drawing.Size(75, 23);
 			this.bNoToAll.TabIndex = 4;
@@ -79,7 +83,7 @@ namespace Fomm.PackageManager {
 			// bNoToFolder
 			// 
 			this.bNoToFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bNoToFolder.Location = new System.Drawing.Point(338, 73);
+			this.bNoToFolder.Location = new System.Drawing.Point(338, 4);
 			this.bNoToFolder.Name = "bNoToFolder";
 			this.bNoToFolder.Size = new System.Drawing.Size(75, 23);
 			this.bNoToFolder.TabIndex = 5;
@@ -90,7 +94,7 @@ namespace Fomm.PackageManager {
 			// bNo
 			// 
 			this.bNo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bNo.Location = new System.Drawing.Point(419, 73);
+			this.bNo.Location = new System.Drawing.Point(419, 4);
 			this.bNo.Name = "bNo";
 			this.bNo.Size = new System.Drawing.Size(75, 23);
 			this.bNo.TabIndex = 6;
@@ -101,12 +105,38 @@ namespace Fomm.PackageManager {
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Location = new System.Drawing.Point(11, 9);
 			this.label1.MinimumSize = new System.Drawing.Size(70, 27);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(70, 27);
 			this.label1.TabIndex = 7;
 			this.label1.Text = "label1";
+			// 
+			// panel1
+			// 
+			this.panel1.AutoSize = true;
+			this.panel1.Controls.Add(this.bNo);
+			this.panel1.Controls.Add(this.bYesToAll);
+			this.panel1.Controls.Add(this.bNoToFolder);
+			this.panel1.Controls.Add(this.bYesToFolder);
+			this.panel1.Controls.Add(this.bNoToAll);
+			this.panel1.Controls.Add(this.bYes);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 69);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(506, 39);
+			this.panel1.TabIndex = 8;
+			// 
+			// panel2
+			// 
+			this.panel2.AutoSize = true;
+			this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(506, 69);
+			this.panel2.TabIndex = 9;
 			// 
 			// Overwriteform
 			// 
@@ -116,17 +146,17 @@ namespace Fomm.PackageManager {
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.ClientSize = new System.Drawing.Size(506, 108);
 			this.ControlBox = false;
-			this.Controls.Add(this.bYesToAll);
-			this.Controls.Add(this.bYesToFolder);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.bNo);
-			this.Controls.Add(this.bYes);
-			this.Controls.Add(this.bNoToAll);
-			this.Controls.Add(this.bNoToFolder);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.KeyPreview = true;
+			this.MinimumSize = new System.Drawing.Size(512, 0);
 			this.Name = "Overwriteform";
 			this.Text = "Confirm Overwrite";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Overwriteform_FormClosing);
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -141,5 +171,7 @@ namespace Fomm.PackageManager {
         private System.Windows.Forms.Button bNoToFolder;
 		private System.Windows.Forms.Button bNo;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
     }
 }
