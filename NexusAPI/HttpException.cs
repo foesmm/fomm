75425e0b54a13f78a01415016ca7bc4429becb12
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-04-22 20:37:25 +0000

    Added an API to interact with Nexus sites.

---
+++ NexusAPI/HttpException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NexusAPI
+{
+	/// <summary>
+	/// The exception that is thrown if an API doesn't receive the expected response to a request.
+	/// </summary>
+	public class HttpException : Exception
+	{
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public HttpException()
+		{
+		}
+
+		/// <summary>
+		/// A simple contructor that sets the exception's message.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		public HttpException(string message)
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// A simple constructor the sets the exception's message and inner exception.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		/// <param name="inner">The ineer exception.</param>
+		public HttpException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
