4cca4f4f8078d37d098c5f9b1f7d75613e19497e
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-06-17 04:51:27 +0000

    Added a control that encapsulates the editing of fomod info.

---
+++ flmm/PackageManager/FomodInfo.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Fomm.PackageManager
+{
+	partial class FomodInfo
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.clbGroups = new System.Windows.Forms.CheckedListBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.tbEmail = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.tbWebsite = new System.Windows.Forms.TextBox();
+			this.tbMinFommVersion = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.tbMVersion = new System.Windows.Forms.TextBox();
+			this.butClearScreenshot = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.tbVersion = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.tbDescription = new System.Windows.Forms.TextBox();
+			this.butSetScreenshot = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.tbAuthor = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tbName = new System.Windows.Forms.TextBox();
+			this.pbxScreenshot = new System.Windows.Forms.PictureBox();
+			this.ofdScreenshot = new System.Windows.Forms.OpenFileDialog();
+			this.erpErrors = new System.Windows.Forms.ErrorProvider(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pbxScreenshot)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.erpErrors)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// clbGroups
+			// 
+			this.clbGroups.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.clbGroups.CheckOnClick = true;
+			this.clbGroups.FormattingEnabled = true;
+			this.clbGroups.IntegralHeight = false;
+			this.clbGroups.Location = new System.Drawing.Point(3, 359);
+			this.clbGroups.Name = "clbGroups";
+			this.clbGroups.Size = new System.Drawing.Size(364, 142);
+			this.clbGroups.TabIndex = 40;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(0, 343);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(41, 13);
+			this.label9.TabIndex = 39;
+			this.label9.Text = "Groups";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(51, 136);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(75, 13);
+			this.label8.TabIndex = 34;
+			this.label8.Text = "Contact Email:";
+			// 
+			// tbEmail
+			// 
+			this.tbEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbEmail.Location = new System.Drawing.Point(132, 133);
+			this.tbEmail.Name = "tbEmail";
+			this.tbEmail.Size = new System.Drawing.Size(219, 20);
+			this.tbEmail.TabIndex = 33;
+			this.tbEmail.Validating += new System.ComponentModel.CancelEventHandler(this.tbEmail_Validating);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(77, 110);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(49, 13);
+			this.label7.TabIndex = 32;
+			this.label7.Text = "Website:";
+			// 
+			// tbWebsite
+			// 
+			this.tbWebsite.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWebsite.Location = new System.Drawing.Point(132, 107);
+			this.tbWebsite.Name = "tbWebsite";
+			this.tbWebsite.Size = new System.Drawing.Size(219, 20);
+			this.tbWebsite.TabIndex = 31;
+			this.tbWebsite.Validating += new System.ComponentModel.CancelEventHandler(this.tbWebsite_Validating);
+			// 
+			// tbMinFommVersion
+			// 
+			this.tbMinFommVersion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbMinFommVersion.Location = new System.Drawing.Point(132, 159);
+			this.tbMinFommVersion.Name = "tbMinFommVersion";
+			this.tbMinFommVersion.Size = new System.Drawing.Size(219, 20);
+			this.tbMinFommVersion.TabIndex = 35;
+			this.tbMinFommVersion.Validating += new System.ComponentModel.CancelEventHandler(this.tbMinFommVersion_Validating);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(0, 162);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(126, 13);
+			this.label6.TabIndex = 36;
+			this.label6.Text = "Required FOMM Version:";
+			// 
+			// tbMVersion
+			// 
+			this.tbMVersion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbMVersion.Location = new System.Drawing.Point(132, 81);
+			this.tbMVersion.Name = "tbMVersion";
+			this.tbMVersion.Size = new System.Drawing.Size(219, 20);
+			this.tbMVersion.TabIndex = 29;
+			this.tbMVersion.Validating += new System.ComponentModel.CancelEventHandler(this.tbMVersion_Validating);
+			// 
+			// butClearScreenshot
+			// 
+			this.butClearScreenshot.Location = new System.Drawing.Point(127, 507);
+			this.butClearScreenshot.Name = "butClearScreenshot";
+			this.butClearScreenshot.Size = new System.Drawing.Size(115, 23);
+			this.butClearScreenshot.TabIndex = 42;
+			this.butClearScreenshot.Text = "Clear Screenshot";
+			this.butClearScreenshot.UseVisualStyleBackColor = true;
+			this.butClearScreenshot.Click += new System.EventHandler(this.butClearScreenshot_Click);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(21, 84);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(105, 13);
+			this.label5.TabIndex = 30;
+			this.label5.Text = "Mod Version (Script):";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(57, 58);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(69, 13);
+			this.label4.TabIndex = 28;
+			this.label4.Text = "Mod Version:";
+			// 
+			// tbVersion
+			// 
+			this.tbVersion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbVersion.Location = new System.Drawing.Point(132, 55);
+			this.tbVersion.Name = "tbVersion";
+			this.tbVersion.Size = new System.Drawing.Size(219, 20);
+			this.tbVersion.TabIndex = 27;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(0, 182);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(84, 13);
+			this.label3.TabIndex = 37;
+			this.label3.Text = "Mod Description";
+			// 
+			// tbDescription
+			// 
+			this.tbDescription.AcceptsReturn = true;
+			this.tbDescription.AcceptsTab = true;
+			this.tbDescription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbDescription.Location = new System.Drawing.Point(3, 198);
+			this.tbDescription.Multiline = true;
+			this.tbDescription.Name = "tbDescription";
+			this.tbDescription.Size = new System.Drawing.Size(364, 142);
+			this.tbDescription.TabIndex = 38;
+			// 
+			// butSetScreenshot
+			// 
+			this.butSetScreenshot.Location = new System.Drawing.Point(6, 507);
+			this.butSetScreenshot.Name = "butSetScreenshot";
+			this.butSetScreenshot.Size = new System.Drawing.Size(115, 23);
+			this.butSetScreenshot.TabIndex = 41;
+			this.butSetScreenshot.Text = "Set Screenshot";
+			this.butSetScreenshot.UseVisualStyleBackColor = true;
+			this.butSetScreenshot.Click += new System.EventHandler(this.butSetScreenshot_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(61, 32);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(65, 13);
+			this.label2.TabIndex = 26;
+			this.label2.Text = "Mod Author:";
+			// 
+			// tbAuthor
+			// 
+			this.tbAuthor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbAuthor.Location = new System.Drawing.Point(132, 29);
+			this.tbAuthor.Name = "tbAuthor";
+			this.tbAuthor.Size = new System.Drawing.Size(219, 20);
+			this.tbAuthor.TabIndex = 25;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(64, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 13);
+			this.label1.TabIndex = 24;
+			this.label1.Text = "Mod Name:";
+			// 
+			// tbName
+			// 
+			this.tbName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbName.Location = new System.Drawing.Point(132, 3);
+			this.tbName.Name = "tbName";
+			this.tbName.Size = new System.Drawing.Size(219, 20);
+			this.tbName.TabIndex = 23;
+			// 
+			// pbxScreenshot
+			// 
+			this.pbxScreenshot.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.pbxScreenshot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbxScreenshot.Location = new System.Drawing.Point(3, 536);
+			this.pbxScreenshot.MinimumSize = new System.Drawing.Size(2, 150);
+			this.pbxScreenshot.Name = "pbxScreenshot";
+			this.pbxScreenshot.Size = new System.Drawing.Size(364, 150);
+			this.pbxScreenshot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pbxScreenshot.TabIndex = 43;
+			this.pbxScreenshot.TabStop = false;
+			// 
+			// ofdScreenshot
+			// 
+			this.ofdScreenshot.FileName = "openFileDialog1";
+			this.ofdScreenshot.Filter = "Image files|*.png;*.jpg;*.bmp";
+			this.ofdScreenshot.RestoreDirectory = true;
+			// 
+			// erpErrors
+			// 
+			this.erpErrors.ContainerControl = this;
+			// 
+			// FomodInfo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.Controls.Add(this.pbxScreenshot);
+			this.Controls.Add(this.clbGroups);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.tbEmail);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.tbWebsite);
+			this.Controls.Add(this.tbMinFommVersion);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.tbMVersion);
+			this.Controls.Add(this.butClearScreenshot);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.tbVersion);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.tbDescription);
+			this.Controls.Add(this.butSetScreenshot);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.tbAuthor);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tbName);
+			this.Name = "FomodInfo";
+			this.Size = new System.Drawing.Size(371, 689);
+			((System.ComponentModel.ISupportInitialize)(this.pbxScreenshot)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.erpErrors)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckedListBox clbGroups;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox tbEmail;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox tbWebsite;
+		private System.Windows.Forms.TextBox tbMinFommVersion;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox tbMVersion;
+		private System.Windows.Forms.Button butClearScreenshot;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox tbVersion;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox tbDescription;
+		private System.Windows.Forms.Button butSetScreenshot;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox tbAuthor;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tbName;
+		private System.Windows.Forms.PictureBox pbxScreenshot;
+		private System.Windows.Forms.OpenFileDialog ofdScreenshot;
+		private System.Windows.Forms.ErrorProvider erpErrors;
+
+	}
+}
---
+++ flmm/PackageManager/FomodInfo.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Fomm.PackageManager
+{
+	/// <summary>
+	/// Encapsulates the editing of FOMOD info.
+	/// </summary>
+	public partial class FomodInfo : UserControl
+	{
+		protected static readonly Regex m_rgxEmail = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", RegexOptions.Singleline | RegexOptions.CultureInvariant);
+
+		private fomod m_fomodMod = null;
+		private byte[] m_bteScreenshot = null;
+		private bool m_booIsValid = true;
+
+		#region Properties
+
+		/// <summary>
+		/// Sets the fomod whose information is being edited.
+		/// </summary>
+		/// <value>The fomod whose information is being edited.</value>
+		public fomod Fomod
+		{
+			set
+			{
+				m_fomodMod = value;
+				tbName.Text = value.Name;
+				tbAuthor.Text = value.Author;
+				tbVersion.Text = value.VersionS;
+				tbMVersion.Text = value.Version.ToString();
+				tbDescription.Text = value.Description;
+				tbWebsite.Text = value.website;
+				tbEmail.Text = value.email;
+				if (value.MinFommVersion == new Version(0, 0, 0, 0))
+					tbMinFommVersion.Text = "";
+				else tbMinFommVersion.Text = value.MinFommVersion.ToString();
+
+				string[] strGroups = Settings.GetStringArray("fomodGroups");
+				clbGroups.SuspendLayout();
+				foreach (string strGroup in strGroups)
+					clbGroups.Items.Add(strGroup, Array.IndexOf<string>(value.groups, strGroup.ToLowerInvariant()) != -1);
+				clbGroups.ResumeLayout();
+
+				pbxScreenshot.Image = value.GetScreenshot();
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the name of the mod.
+		/// </summary>
+		/// <value>The name of the mod.</value>
+		public string ModName
+		{
+			get
+			{
+				return tbName.Text;
+			}
+			set
+			{
+				tbName.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the author of the mod.
+		/// </summary>
+		/// <value>The author of the mod.</value>
+		public string Author
+		{
+			get
+			{
+				return tbAuthor.Text;
+			}
+			set
+			{
+				tbAuthor.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the human readable Version of the mod.
+		/// </summary>
+		/// <value>The human readable Version of the mod.</value>
+		public string Version
+		{
+			get
+			{
+				return tbVersion.Text;
+			}
+			set
+			{
+				tbVersion.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the machine Version of the mod.
+		/// </summary>
+		/// <value>The machine Version of the mod.</value>
+		public string MachineVersion
+		{
+			get
+			{
+				return tbMVersion.Text;
+			}
+			set
+			{
+				tbMVersion.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the Description of the mod.
+		/// </summary>
+		/// <value>The Description of the mod.</value>
+		public string Description
+		{
+			get
+			{
+				return tbDescription.Text;
+			}
+			set
+			{
+				tbDescription.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the Website of the mod.
+		/// </summary>
+		/// <value>The Website of the mod.</value>
+		public string Website
+		{
+			get
+			{
+				return tbWebsite.Text;
+			}
+			set
+			{
+				tbWebsite.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the Email of the mod.
+		/// </summary>
+		/// <value>The Email of the mod.</value>
+		public string Email
+		{
+			get
+			{
+				return tbEmail.Text;
+			}
+			set
+			{
+				tbEmail.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the required FOMM version of the mod.
+		/// </summary>
+		/// <value>The required FOMM version of the mod.</value>
+		public string RequiredFOMMVersion
+		{
+			get
+			{
+				return tbMinFommVersion.Text;
+			}
+			set
+			{
+				tbMinFommVersion.Text = value;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public FomodInfo()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Screenshot
+
+		/// <summary>
+		/// Handles the <see cref="Control.CLick"/> event of the set screenshot button.
+		/// </summary>
+		/// <remarks>
+		/// Sets the screenshot for the fomod.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void butSetScreenshot_Click(object sender, EventArgs e)
+		{
+			if (ofdScreenshot.ShowDialog() == DialogResult.OK)
+			{
+				m_bteScreenshot = File.ReadAllBytes(ofdScreenshot.FileName);
+				pbxScreenshot.Image = Bitmap.FromFile(ofdScreenshot.FileName);
+			}
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.CLick"/> event of the clear screenshot button.
+		/// </summary>
+		/// <remarks>
+		/// Removes the screenshot from the fomod.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void butClearScreenshot_Click(object sender, EventArgs e)
+		{
+			pbxScreenshot.Image = null;
+			m_bteScreenshot = null;
+		}
+
+		#endregion
+
+		#region Validation
+
+		/// <summary>
+		/// Handles the <see cref="Control.Validating"/> event of the machine version textbox.
+		/// </summary>
+		/// <remarks>
+		/// Ensures that the version is valid, if present.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="CancelEventArgs"/> describing the event arguments.</param>
+		private void tbMVersion_Validating(object sender, CancelEventArgs e)
+		{
+			erpErrors.SetError(tbMVersion, null);
+			if (!String.IsNullOrEmpty(tbMVersion.Text))
+			{
+				try
+				{
+					new Version(tbMVersion.Text);
+				}
+				catch
+				{
+					erpErrors.SetError(tbMVersion, "Invalid version. Must be in #.#.#.# format.");
+					m_booIsValid = false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Validating"/> event of the website textbox.
+		/// </summary>
+		/// <remarks>
+		/// Ensures that the URL is valid, if present.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="CancelEventArgs"/> describing the event arguments.</param>
+		private void tbWebsite_Validating(object sender, CancelEventArgs e)
+		{
+			erpErrors.SetError(tbWebsite, null);
+			if (!String.IsNullOrEmpty(tbWebsite.Text))
+			{
+				Uri uri;
+				if (!Uri.TryCreate(tbWebsite.Text, UriKind.Absolute, out uri) || uri.IsFile || (uri.Scheme != "http" && uri.Scheme != "https"))
+				{
+					erpErrors.SetError(tbWebsite, "Invalid web address specified.\nDid you miss the 'http://'?");
+					m_booIsValid = false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Validating"/> event of the minimum FOMM version textbox.
+		/// </summary>
+		/// <remarks>
+		/// Ensures that the version is valid, if present.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="CancelEventArgs"/> describing the event arguments.</param>
+		private void tbMinFommVersion_Validating(object sender, CancelEventArgs e)
+		{
+			erpErrors.SetError(tbMinFommVersion, null);
+			if (!String.IsNullOrEmpty(tbMinFommVersion.Text))
+			{
+				Version verVersion = null;
+				try
+				{
+					verVersion = new Version(tbMinFommVersion.Text);
+				}
+				catch
+				{
+					erpErrors.SetError(tbMinFommVersion, "Invalid version. Must be in #.#.#.# format.");
+					m_booIsValid = false;
+					return;
+				}
+				if (verVersion > Program.MVersion)
+				{
+					erpErrors.SetError(tbMinFommVersion, "Specified version is newer than this version of FOMM.");
+					m_booIsValid = false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Validating"/> event of the email textbox.
+		/// </summary>
+		/// <remarks>
+		/// Ensures that the email is valid, if present.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="CancelEventArgs"/> describing the event arguments.</param>
+		private void tbEmail_Validating(object sender, CancelEventArgs e)
+		{
+			erpErrors.SetError(tbEmail, null);
+			if (!String.IsNullOrEmpty(tbEmail.Text) && !m_rgxEmail.IsMatch(tbEmail.Text))
+			{
+				erpErrors.SetError(tbEmail, "Invalid email address.");
+				m_booIsValid = false;
+			}
+		}
+
+		/// <summary>
+		/// Validate the controls on this control.
+		/// </summary>
+		/// <returns><lang cref="true"/> if all controls passed validation; <lang cref="false"/> otherwise.</returns>
+		public bool PerformValidation()
+		{
+			m_booIsValid=true;
+			this.ValidateChildren();
+			return m_booIsValid;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Saves the edited info to the fomod being edited.
+		/// </summary>
+		/// <returns><lang cref="false"/> if the info failed validation and was not saved;
+		/// <lang cref="true"/> otherwise.</returns>
+		/// <exception cref="InvalidOperationException">Thrown if the method is called before <see cref="Fomod"/> is set.</exception>
+		public bool SaveInfo()
+		{
+			if (m_fomodMod == null)
+				throw new InvalidOperationException("Save() cannot be called before setting the Fomod being edited.");
+
+			if (!this.ValidateChildren())
+				return false;
+
+			if (!String.IsNullOrEmpty(tbVersion.Text) && String.IsNullOrEmpty(tbMVersion.Text))
+			{
+				erpErrors.SetError(tbMVersion, "Machine must be specified if Version is specified.");
+				return false;
+			}
+
+			m_fomodMod.Name = tbName.Text;
+			m_fomodMod.Author = tbAuthor.Text;
+			m_fomodMod.VersionS = tbVersion.Text;
+			m_fomodMod.Description = tbDescription.Text;
+			m_fomodMod.website = tbWebsite.Text;
+			m_fomodMod.email = tbEmail.Text;
+			if (!String.IsNullOrEmpty(tbMinFommVersion.Text))
+				m_fomodMod.MinFommVersion = new Version(tbMinFommVersion.Text);
+			if (!String.IsNullOrEmpty(tbMVersion.Text))
+				m_fomodMod.Version = new Version(tbMVersion.Text);
+
+			m_fomodMod.groups = new string[clbGroups.CheckedItems.Count];
+			for (Int32 i = 0; i < m_fomodMod.groups.Length; i++)
+				m_fomodMod.groups[i] = ((string)clbGroups.CheckedItems[i]).ToLowerInvariant();
+
+			m_fomodMod.CommitInfo(pbxScreenshot.Image != null, m_bteScreenshot);
+
+			return true;
+		}
+
+		/// <summary>
+		/// Raises the <see cref="Control.Resize"/> event.
+		/// </summary>
+		/// <remarks>
+		/// This handle the resizing of the screenshot piture box, as anchoring it screws up the
+		/// autoscrolling (don't know why).
+		/// </remarks>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+			pbxScreenshot.Height = this.ClientSize.Height - pbxScreenshot.Top - 3;
+			pbxScreenshot.Width = this.ClientSize.Width - 6;
+		}
+	}
+}
