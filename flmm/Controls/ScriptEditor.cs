49a355a0492075d03be241e5761e9a1f256658e0
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-06-17 04:49:26 +0000

    Added an editor for FOMOD C# scripts.

---
+++ flmm/Controls/ScriptEditor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fomm.Controls
+{
+	/// <summary>
+	/// Text editor for the script of a fomod.
+	/// </summary>
+	public partial class ScriptEditor : UserControl
+	{
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ScriptEditor()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Checks the syntax of the script.
+		/// </summary>
+		protected void CheckSyntax()
+		{
+			string stdout;
+			string errors = Fomm.PackageManager.ScriptCompiler.CheckSyntax(cedEditor.Text, out stdout);
+			if (errors != null)
+			{
+				MessageBox.Show(errors);
+			}
+			else
+			{
+				MessageBox.Show("No errors found");
+			}
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> event of the check syntax button.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event argument.</param>
+		private void tsbCheckSyntax_Click(object sender, EventArgs e)
+		{
+			CheckSyntax();
+		}
+	}
+}
