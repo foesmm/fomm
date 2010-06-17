44021d90113fceed24e25d4395e6794512f0f851
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-06-17 04:51:43 +0000

    Added a Set implementation.

---
+++ flmm/Util/Set.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fomm.Util
+{
+	/// <summary>
+	/// A Set implementation.
+	/// </summary>
+	/// <typeparam name="T">The type of objects in the Set.</typeparam>
+	public class Set<T> : List<T>
+	{
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public Set()
+		{
+		}
+
+		/// <summary>
+		/// The copy constructor.
+		/// </summary>
+		/// <param name="p_setCopy">The set to copy.</param>
+		public Set(Set<T> p_setCopy)
+			: base(p_setCopy)
+		{
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Adds the given item to the set.
+		/// </summary>
+		/// <param name="p_tItem">the item to add.</param>
+		public new void Add(T p_tItem)
+		{
+			if (!Contains(p_tItem))
+				base.Add(p_tItem);
+		}
+	}
+}
