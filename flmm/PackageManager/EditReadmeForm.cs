578ff91b3bf14a157bcdff458dda30c8218b25b3
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-06-28 19:30:45 +0000

    Added persistance of window location.

--- flmm/PackageManager/EditReadmeForm.cs
+++ flmm/PackageManager/EditReadmeForm.cs
@@ -38,10 +38,31 @@ namespace Fomm.PackageManager
 		public EditReadmeForm()
 		{
 			InitializeComponent();
+
+			Icon = Fomm.Properties.Resources.fomm02;
+			Settings.GetWindowPosition("EditReadmeForm", this);
 		}
 
 		#endregion
 
+		/// <summary>
+		/// Raises the <see cref="Form.Closing"/> event.
+		/// </summary>
+		/// <remarks>
+		/// Saves the window's position.
+		/// </remarks>
+		/// <param name="e">A <see cref="CancelEventArgs"/> describing the event arguments.</param>
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			Settings.SetWindowPosition("EditReadmeForm", this);
+			base.OnClosing(e);
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> event of the OK button.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
 		private void butOK_Click(object sender, EventArgs e)
 		{
 			DialogResult = DialogResult.OK;
