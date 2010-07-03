5b201aa7ec9dc86086ea48c797790446d6abd73a
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-07-03 00:20:18 +0000

    Updated comments.

--- flmm/PackageManager/FomodBuilder/SourceDownloadSelector.cs
+++ flmm/PackageManager/FomodBuilder/SourceDownloadSelector.cs
@@ -7,8 +7,20 @@ using System.ComponentModel;
 
 namespace Fomm.PackageManager.FomodBuilder
 {
+	/// <summary>
+	/// A control that allows the specification of download locations for source files.
+	/// </summary>
+	/// <remarks>
+	/// Source files are used to build FOMods and PFPs.
+	/// </remarks>
 	public partial class SourceDownloadSelector : UserControl
 	{
+		/// <summary>
+		/// Encasuplates the download location of a source file.
+		/// </summary>
+		/// <remarks>
+		/// Source files are used to build FOMods and PFPs.
+		/// </remarks>
 		public class SourceDownloadLocation
 		{
 			public string Source { get; set; }
@@ -36,6 +48,12 @@ namespace Fomm.PackageManager.FomodBuilder
 			}
 		}
 
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the list of source download locations being managed.
+		/// </summary>
+		/// <value>The list of source download locations being managed.</value>
 		[Browsable(false)]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
 		public IList<SourceDownloadLocation> DataSource
@@ -50,17 +68,41 @@ namespace Fomm.PackageManager.FomodBuilder
 			}
 		}
 
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
 		public SourceDownloadSelector()
 		{
 			InitializeComponent();
 		}
 
+		#endregion
+
+		/// <summary>
+		/// Handles the <see cref="DataGridView.CurrentCellDirtyStateChanged"/> event of the
+		/// source list grid.
+		/// </summary>
+		/// <remarks>
+		/// This commits changes.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
 		private void dgvSourceList_CurrentCellDirtyStateChanged(object sender, EventArgs e)
 		{
 			if (dgvSourceList.IsCurrentCellDirty)
 				dgvSourceList.CommitEdit(DataGridViewDataErrorContexts.Commit);
 		}
 
+		/// <summary>
+		/// Handles the <see cref="DataGridView.CellValueChanged"/> event of the
+		/// source list grid.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="DataGridViewCellEventArgs"/> describing the event arguments.</param>
 		private void dgvSourceList_CellValueChanged(object sender, DataGridViewCellEventArgs e)
 		{
 			if (dgvSourceList.Columns[e.ColumnIndex].Name == "clmIncluded")
@@ -70,6 +112,11 @@ namespace Fomm.PackageManager.FomodBuilder
 			}
 		}
 
+		/// <summary>
+		/// This changes the appearance of the specified row dependent upon whether the source
+		/// has been marked as included.
+		/// </summary>
+		/// <param name="p_intRowIndex"></param>
 		private void HandleIncludedChange(Int32 p_intRowIndex)
 		{
 			DataGridViewTextBoxCell downloadLocationCell = (DataGridViewTextBoxCell)dgvSourceList.Rows[p_intRowIndex].Cells["clmURL"];
@@ -81,6 +128,12 @@ namespace Fomm.PackageManager.FomodBuilder
 
 		}
 
+		/// <summary>
+		/// Handles the <see cref="DataGridView.RowsAdded"/> event of the
+		/// source list grid.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="DataGridViewRowsAddedEventArgs"/> describing the event arguments.</param>
 		private void dgvSourceList_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
 		{
 			HandleIncludedChange(e.RowIndex);
