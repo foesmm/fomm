787ed719653619d8fd28b70c561058f83536ec57
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-11-02 22:26:25 +0000

    Refactored to remove last traces of hard-coded "data" path. Refactored to separate out auto sorter code.

---
+++ flmm/Games/PluginInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+namespace Fomm.Games
+{
+	/// <summary>
+	/// Encapsulates the information about a plugin.
+	/// </summary>
+	public class PluginInfo
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the description of the plugin.
+		/// </summary>
+		/// <value>The description of the plugin.</value>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Gets or sets the image of the plugin.
+		/// </summary>
+		/// <value>The picture of the plugin.</value>
+		public Image Picture { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strDescription">The description of the plugin.</param>
+		/// <param name="p_imgPicture">The picture of the plugin.</param>
+		public PluginInfo(string p_strDescription, Image p_imgPicture)
+		{
+			Description = p_strDescription;
+			Picture = p_imgPicture;
+		}
+
+		#endregion
+	}
+}
