2b2338e2d972aad81b83d076d206529d957debcb
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-11-12 09:50:42 +0000

    Incremented version to 0.13.12

--- flmm/Program.cs
+++ flmm/Program.cs
@@ -57,7 +57,7 @@ namespace Fomm
 
 	public static class Program
 	{
-		public const string Version = "0.13.11";
+		public const string Version = "0.13.12";
 		public static readonly Version MVersion = new Version(Version + ".0");
 
 #if TRACE
