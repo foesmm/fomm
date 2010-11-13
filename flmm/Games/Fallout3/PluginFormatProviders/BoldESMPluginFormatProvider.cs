be4a6aaca499ff00f459e68cd3d35b16cf43c32a
kaburke <kaburke@727d929f-1e04-4230-bf68-c6d161abaee7>
2010-11-13 06:42:48 +0000

    Put files in the correct directories (they were swapped), and changed from checking extension to checking ESM flag in the header.

--- flmm/Games/Fallout3/PluginFormatProviders/BoldESMPluginFormatProvider.cs
+++ flmm/Games/Fallout3/PluginFormatProviders/BoldESMPluginFormatProvider.cs
@@ -1,22 +1,53 @@
 ﻿using System;
 using System.IO;
+using System.Drawing;
+using Fomm.Games.Fallout3.Tools.TESsnip;
 
-namespace Fomm.Games.FalloutNewVegas.PluginFormatProviders
+namespace Fomm.Games.Fallout3.PluginFormatProviders
 {
 	/// <summary>
 	/// A plugin format provider that makes ESM plugins bold if the plugin list.
 	/// </summary>
-	public class BoldESMPluginFormatProvider : Fallout3.PluginFormatProviders.BoldESMPluginFormatProvider
+	public class BoldESMPluginFormatProvider : IPluginFormatProvider
 	{
+		private PluginFormat.PluginFormatterManager m_pfmManager = null;
+
+		#region IPluginFormatProvider Members
+
+		/// <summary>
+		/// Sets the <see cref="PluginFormat.PluginFormatterManager"/> to use.
+		/// </summary>
+		/// <value>The <see cref="PluginFormat.PluginFormatterManager"/> to use.</value>
+		public PluginFormat.PluginFormatterManager PluginFormatterManager
+		{
+			set
+			{
+				m_pfmManager = value;
+			}
+		}
+
 		/// <summary>
 		/// Determins if the provider has a format for the specified plugin.
 		/// </summary>
 		/// <param name="p_strPluginName">The name of the plugin for which to check if there is a format.</param>
 		/// <returns><lang cref="true"/> if this provider has a format for the specified plugin;
 		/// <lang cref="false"/> otherwise.</returns>
-		public override bool HasFormat(string p_strPluginName)
+		public virtual bool HasFormat(string p_strPluginName)
 		{
-			return Properties.Settings.Default.falloutNewVegasBoldifyESMs && Path.GetExtension(p_strPluginName).Equals(".esm", StringComparison.InvariantCultureIgnoreCase);
+			return Properties.Settings.Default.fallout3BoldifyESMs && Plugin.GetIsEsm(p_strPluginName);
 		}
+
+		/// <summary>
+		/// Gets the provider's format for the specified plugin.
+		/// </summary>
+		/// <param name="p_strPluginName">The name of the plugin whose format is to be returned.</param>
+		/// <returns>The provider's format for the specified plugin, or <lang cref="null"/> if the
+		/// provider does not have a format for the speficied plugin.</returns>
+		public PluginFormat GetFormat(string p_strPluginName)
+		{
+			return m_pfmManager.CreateFormat(null, null, FontStyle.Bold, null, null, null);
+		}
+
+		#endregion
 	}
 }
--- flmm/Games/FalloutNewVegas/PluginFormatProviders/BoldESMPluginFormatProvider.cs
+++ flmm/Games/FalloutNewVegas/PluginFormatProviders/BoldESMPluginFormatProvider.cs
@@ -1,53 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
+using Fomm.Games.Fallout3.Tools.TESsnip;
 
-namespace Fomm.Games.Fallout3.PluginFormatProviders
+namespace Fomm.Games.FalloutNewVegas.PluginFormatProviders
 {
 	/// <summary>
 	/// A plugin format provider that makes ESM plugins bold if the plugin list.
 	/// </summary>
-	public class BoldESMPluginFormatProvider : IPluginFormatProvider
+	public class BoldESMPluginFormatProvider : Fallout3.PluginFormatProviders.BoldESMPluginFormatProvider
 	{
-		private PluginFormat.PluginFormatterManager m_pfmManager = null;
-		
-		#region IPluginFormatProvider Members
-
-		/// <summary>
-		/// Sets the <see cref="PluginFormat.PluginFormatterManager"/> to use.
-		/// </summary>
-		/// <value>The <see cref="PluginFormat.PluginFormatterManager"/> to use.</value>
-		public PluginFormat.PluginFormatterManager PluginFormatterManager
-		{
-			set
-			{
-				m_pfmManager = value;
-			}
-		}
-
 		/// <summary>
 		/// Determins if the provider has a format for the specified plugin.
 		/// </summary>
 		/// <param name="p_strPluginName">The name of the plugin for which to check if there is a format.</param>
 		/// <returns><lang cref="true"/> if this provider has a format for the specified plugin;
 		/// <lang cref="false"/> otherwise.</returns>
-		public virtual bool HasFormat(string p_strPluginName)
+		public override bool HasFormat(string p_strPluginName)
 		{
-			return Properties.Settings.Default.fallout3BoldifyESMs && Path.GetExtension(p_strPluginName).Equals(".esm", StringComparison.InvariantCultureIgnoreCase);
+			return Properties.Settings.Default.falloutNewVegasBoldifyESMs && Plugin.GetIsEsm(p_strPluginName);
 		}
-
-		/// <summary>
-		/// Gets the provider's format for the specified plugin.
-		/// </summary>
-		/// <param name="p_strPluginName">The name of the plugin whose format is to be returned.</param>
-		/// <returns>The provider's format for the specified plugin, or <lang cref="null"/> if the
-		/// provider does not have a format for the speficied plugin.</returns>
-		public PluginFormat GetFormat(string p_strPluginName)
-		{
-			return m_pfmManager.CreateFormat(null, null, FontStyle.Bold, null, null, null);
-		}
-
-		#endregion
 	}
 }
+
