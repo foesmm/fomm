e6db594568ba40b8a35d8596cd20cd30bfdf84b5
prideslayer <pride3270@gmail.com>
2014-02-16 19:51:59 -0500

    Form resizing now improved to work properly.

--- flmm/PackageManager/Overwriteform.Designer.cs
+++ flmm/PackageManager/Overwriteform.Designer.cs
@@ -25,21 +25,21 @@ namespace Fomm.PackageManager {
         private void InitializeComponent() {
       this.label1 = new System.Windows.Forms.Label();
       this.panel2 = new System.Windows.Forms.Panel();
-      this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+      this.panel1 = new System.Windows.Forms.Panel();
       this.panel3 = new System.Windows.Forms.Panel();
-      this.panel4 = new System.Windows.Forms.Panel();
-      this.panel5 = new System.Windows.Forms.Panel();
-      this.panel6 = new System.Windows.Forms.Panel();
       this.bNo = new System.Windows.Forms.Button();
       this.bYes = new System.Windows.Forms.Button();
+      this.panel4 = new System.Windows.Forms.Panel();
       this.bNoToMod = new System.Windows.Forms.Button();
       this.bYesToMod = new System.Windows.Forms.Button();
+      this.panel5 = new System.Windows.Forms.Panel();
       this.bNoToFolder = new System.Windows.Forms.Button();
       this.bYesToFolder = new System.Windows.Forms.Button();
+      this.panel6 = new System.Windows.Forms.Panel();
       this.bYesToAll = new System.Windows.Forms.Button();
       this.bNoToAll = new System.Windows.Forms.Button();
       this.panel2.SuspendLayout();
-      this.flowLayoutPanel1.SuspendLayout();
+      this.panel1.SuspendLayout();
       this.panel3.SuspendLayout();
       this.panel4.SuspendLayout();
       this.panel5.SuspendLayout();
@@ -66,134 +66,154 @@ namespace Fomm.PackageManager {
       this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
       this.panel2.Location = new System.Drawing.Point(0, 0);
       this.panel2.Name = "panel2";
-      this.panel2.Size = new System.Drawing.Size(470, 233);
+      this.panel2.Size = new System.Drawing.Size(440, 233);
       this.panel2.TabIndex = 9;
       // 
-      // flowLayoutPanel1
+      // panel1
       // 
-      this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-      this.flowLayoutPanel1.Controls.Add(this.panel3);
-      this.flowLayoutPanel1.Controls.Add(this.panel4);
-      this.flowLayoutPanel1.Controls.Add(this.panel5);
-      this.flowLayoutPanel1.Controls.Add(this.panel6);
-      this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-      this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 164);
-      this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-      this.flowLayoutPanel1.Size = new System.Drawing.Size(470, 69);
-      this.flowLayoutPanel1.TabIndex = 10;
+      this.panel1.Controls.Add(this.panel3);
+      this.panel1.Controls.Add(this.panel4);
+      this.panel1.Controls.Add(this.panel5);
+      this.panel1.Controls.Add(this.panel6);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel1.Location = new System.Drawing.Point(0, 162);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(440, 71);
+      this.panel1.TabIndex = 10;
+      this.panel1.Layout += new System.Windows.Forms.LayoutEventHandler(this.panel1_Layout);
       // 
       // panel3
       // 
       this.panel3.Controls.Add(this.bNo);
       this.panel3.Controls.Add(this.bYes);
-      this.panel3.Location = new System.Drawing.Point(3, 3);
+      this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel3.Location = new System.Drawing.Point(330, 0);
       this.panel3.Name = "panel3";
-      this.panel3.Size = new System.Drawing.Size(111, 63);
-      this.panel3.TabIndex = 0;
-      // 
-      // panel4
-      // 
-      this.panel4.Controls.Add(this.bNoToMod);
-      this.panel4.Controls.Add(this.bYesToMod);
-      this.panel4.Location = new System.Drawing.Point(120, 3);
-      this.panel4.Name = "panel4";
-      this.panel4.Size = new System.Drawing.Size(111, 63);
-      this.panel4.TabIndex = 1;
-      // 
-      // panel5
-      // 
-      this.panel5.Controls.Add(this.bNoToFolder);
-      this.panel5.Controls.Add(this.bYesToFolder);
-      this.panel5.Location = new System.Drawing.Point(237, 3);
-      this.panel5.Name = "panel5";
-      this.panel5.Size = new System.Drawing.Size(111, 63);
-      this.panel5.TabIndex = 2;
-      // 
-      // panel6
-      // 
-      this.panel6.Controls.Add(this.bYesToAll);
-      this.panel6.Controls.Add(this.bNoToAll);
-      this.panel6.Location = new System.Drawing.Point(354, 3);
-      this.panel6.Name = "panel6";
-      this.panel6.Size = new System.Drawing.Size(112, 63);
-      this.panel6.TabIndex = 3;
+      this.panel3.Size = new System.Drawing.Size(110, 71);
+      this.panel3.TabIndex = 4;
       // 
       // bNo
       // 
-      this.bNo.Location = new System.Drawing.Point(18, 32);
+      this.bNo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bNo.Location = new System.Drawing.Point(18, 37);
       this.bNo.Name = "bNo";
       this.bNo.Size = new System.Drawing.Size(75, 23);
-      this.bNo.TabIndex = 4;
+      this.bNo.TabIndex = 2;
       this.bNo.Text = "No";
       this.bNo.UseVisualStyleBackColor = true;
       this.bNo.Click += new System.EventHandler(this.bNo_Click);
       // 
       // bYes
       // 
-      this.bYes.Location = new System.Drawing.Point(18, 3);
+      this.bYes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bYes.Location = new System.Drawing.Point(18, 8);
       this.bYes.Name = "bYes";
       this.bYes.Size = new System.Drawing.Size(75, 23);
-      this.bYes.TabIndex = 3;
+      this.bYes.TabIndex = 1;
       this.bYes.Text = "Yes";
       this.bYes.UseVisualStyleBackColor = true;
       this.bYes.Click += new System.EventHandler(this.bYes_Click);
       // 
+      // panel4
+      // 
+      this.panel4.Controls.Add(this.bNoToMod);
+      this.panel4.Controls.Add(this.bYesToMod);
+      this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+      this.panel4.Location = new System.Drawing.Point(220, 0);
+      this.panel4.Name = "panel4";
+      this.panel4.Size = new System.Drawing.Size(110, 71);
+      this.panel4.TabIndex = 5;
+      // 
       // bNoToMod
       // 
-      this.bNoToMod.Location = new System.Drawing.Point(18, 32);
+      this.bNoToMod.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bNoToMod.Location = new System.Drawing.Point(18, 37);
       this.bNoToMod.Name = "bNoToMod";
       this.bNoToMod.Size = new System.Drawing.Size(75, 23);
-      this.bNoToMod.TabIndex = 6;
+      this.bNoToMod.TabIndex = 4;
       this.bNoToMod.Text = "No to mod";
       this.bNoToMod.UseVisualStyleBackColor = true;
       this.bNoToMod.Click += new System.EventHandler(this.bNoToMod_Click);
       // 
       // bYesToMod
       // 
-      this.bYesToMod.Location = new System.Drawing.Point(18, 3);
+      this.bYesToMod.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bYesToMod.Location = new System.Drawing.Point(18, 8);
       this.bYesToMod.Name = "bYesToMod";
       this.bYesToMod.Size = new System.Drawing.Size(75, 23);
-      this.bYesToMod.TabIndex = 5;
+      this.bYesToMod.TabIndex = 3;
       this.bYesToMod.Text = "Yes to mod";
       this.bYesToMod.UseVisualStyleBackColor = true;
       this.bYesToMod.Click += new System.EventHandler(this.bYesToMod_Click);
       // 
+      // panel5
+      // 
+      this.panel5.Controls.Add(this.bNoToFolder);
+      this.panel5.Controls.Add(this.bYesToFolder);
+      this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+      this.panel5.Location = new System.Drawing.Point(110, 0);
+      this.panel5.Name = "panel5";
+      this.panel5.Size = new System.Drawing.Size(110, 71);
+      this.panel5.TabIndex = 6;
+      // 
       // bNoToFolder
       // 
-      this.bNoToFolder.Location = new System.Drawing.Point(18, 32);
+      this.bNoToFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bNoToFolder.Location = new System.Drawing.Point(18, 37);
       this.bNoToFolder.Name = "bNoToFolder";
       this.bNoToFolder.Size = new System.Drawing.Size(75, 23);
-      this.bNoToFolder.TabIndex = 8;
+      this.bNoToFolder.TabIndex = 6;
       this.bNoToFolder.Text = "No to folder";
       this.bNoToFolder.UseVisualStyleBackColor = true;
       this.bNoToFolder.Click += new System.EventHandler(this.bNoToFolder_Click);
       // 
       // bYesToFolder
       // 
-      this.bYesToFolder.Location = new System.Drawing.Point(18, 3);
+      this.bYesToFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bYesToFolder.Location = new System.Drawing.Point(18, 8);
       this.bYesToFolder.Name = "bYesToFolder";
       this.bYesToFolder.Size = new System.Drawing.Size(75, 23);
-      this.bYesToFolder.TabIndex = 7;
+      this.bYesToFolder.TabIndex = 5;
       this.bYesToFolder.Text = "Yes to folder";
       this.bYesToFolder.UseVisualStyleBackColor = true;
       this.bYesToFolder.Click += new System.EventHandler(this.bYesToFolder_Click);
       // 
+      // panel6
+      // 
+      this.panel6.Controls.Add(this.bYesToAll);
+      this.panel6.Controls.Add(this.bNoToAll);
+      this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
+      this.panel6.Location = new System.Drawing.Point(0, 0);
+      this.panel6.Name = "panel6";
+      this.panel6.Size = new System.Drawing.Size(110, 71);
+      this.panel6.TabIndex = 3;
+      // 
       // bYesToAll
       // 
-      this.bYesToAll.Location = new System.Drawing.Point(18, 3);
+      this.bYesToAll.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bYesToAll.Location = new System.Drawing.Point(18, 8);
       this.bYesToAll.Name = "bYesToAll";
       this.bYesToAll.Size = new System.Drawing.Size(75, 23);
-      this.bYesToAll.TabIndex = 9;
+      this.bYesToAll.TabIndex = 7;
       this.bYesToAll.Text = "Yes to all";
       this.bYesToAll.UseVisualStyleBackColor = true;
       this.bYesToAll.Click += new System.EventHandler(this.bYesToAll_Click);
       // 
       // bNoToAll
       // 
-      this.bNoToAll.Location = new System.Drawing.Point(18, 32);
+      this.bNoToAll.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.bNoToAll.Location = new System.Drawing.Point(18, 37);
       this.bNoToAll.Name = "bNoToAll";
       this.bNoToAll.Size = new System.Drawing.Size(75, 23);
-      this.bNoToAll.TabIndex = 10;
+      this.bNoToAll.TabIndex = 8;
       this.bNoToAll.Text = "No to all";
       this.bNoToAll.UseVisualStyleBackColor = true;
       this.bNoToAll.Click += new System.EventHandler(this.bNoToAll_Click);
@@ -202,18 +222,18 @@ namespace Fomm.PackageManager {
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(470, 233);
+      this.ClientSize = new System.Drawing.Size(440, 233);
       this.ControlBox = false;
-      this.Controls.Add(this.flowLayoutPanel1);
+      this.Controls.Add(this.panel1);
       this.Controls.Add(this.panel2);
       this.KeyPreview = true;
-      this.MinimumSize = new System.Drawing.Size(486, 200);
+      this.MinimumSize = new System.Drawing.Size(400, 200);
       this.Name = "Overwriteform";
       this.Text = "Confirm Overwrite";
       this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Overwriteform_FormClosing);
       this.panel2.ResumeLayout(false);
       this.panel2.PerformLayout();
-      this.flowLayoutPanel1.ResumeLayout(false);
+      this.panel1.ResumeLayout(false);
       this.panel3.ResumeLayout(false);
       this.panel4.ResumeLayout(false);
       this.panel5.ResumeLayout(false);
@@ -226,7 +246,7 @@ namespace Fomm.PackageManager {
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button bNo;
         private System.Windows.Forms.Button bYes;
--- flmm/PackageManager/Overwriteform.cs
+++ flmm/PackageManager/Overwriteform.cs
@@ -3,72 +3,72 @@ using System.Windows.Forms;
 
 namespace Fomm.PackageManager
 {
-	enum OverwriteResult { YesToAll = 1, YesToFolder = 2, Yes = 3, NoToAll = 4, NoToFolder = 5, No = 6, YesToMod = 7, NoToMod = 8 }
+  enum OverwriteResult { YesToAll = 1, YesToFolder = 2, Yes = 3, NoToAll = 4, NoToFolder = 5, No = 6, YesToMod = 7, NoToMod = 8 }
 
-	partial class Overwriteform : Form
-	{
-		private Overwriteform(string msg, bool allowFolder, bool allowMod)
-		{
-			InitializeComponent();
-			this.Icon = Fomm.Properties.Resources.fomm02;
+  partial class Overwriteform : Form
+  {
+    private Overwriteform(string msg, bool allowFolder, bool allowMod)
+    {
+      InitializeComponent();
+      this.Icon = Fomm.Properties.Resources.fomm02;
       Properties.Settings.Default.windowPositions.GetWindowPosition("OverwriteForm", this);
       label1.Text = msg;
-			if (!allowFolder)
-			{
-				bYesToFolder.Enabled = false;
-				bNoToFolder.Enabled = false;
-			}
+      if (!allowFolder)
+      {
+        bYesToFolder.Enabled = false;
+        bNoToFolder.Enabled = false;
+      }
       if (!allowMod)
       {
         bYesToMod.Enabled = false;
         bNoToMod.Enabled = false;
       }
-		}
+    }
 
-		private OverwriteResult result;
+    private OverwriteResult result;
 
-		public static OverwriteResult ShowDialog(string msg, bool allowFolder, bool allowMod)
-		{
-			Overwriteform of = new Overwriteform(msg, allowFolder, allowMod);
-			of.ShowDialog();
-			return of.result;
-		}
+    public static OverwriteResult ShowDialog(string msg, bool allowFolder, bool allowMod)
+    {
+      Overwriteform of = new Overwriteform(msg, allowFolder, allowMod);
+      of.ShowDialog();
+      return of.result;
+    }
 
-		private void bYesToAll_Click(object sender, EventArgs e)
-		{
-			result = OverwriteResult.YesToAll;
-			Close();
-		}
+    private void bYesToAll_Click(object sender, EventArgs e)
+    {
+      result = OverwriteResult.YesToAll;
+      Close();
+    }
 
-		private void bYesToFolder_Click(object sender, EventArgs e)
-		{
-			result = OverwriteResult.YesToFolder;
-			Close();
-		}
+    private void bYesToFolder_Click(object sender, EventArgs e)
+    {
+      result = OverwriteResult.YesToFolder;
+      Close();
+    }
 
-		private void bYes_Click(object sender, EventArgs e)
-		{
-			result = OverwriteResult.Yes;
-			Close();
-		}
+    private void bYes_Click(object sender, EventArgs e)
+    {
+      result = OverwriteResult.Yes;
+      Close();
+    }
 
-		private void bNoToAll_Click(object sender, EventArgs e)
-		{
-			result = OverwriteResult.NoToAll;
-			Close();
-		}
+    private void bNoToAll_Click(object sender, EventArgs e)
+    {
+      result = OverwriteResult.NoToAll;
+      Close();
+    }
 
-		private void bNoToFolder_Click(object sender, EventArgs e)
-		{
-			result = OverwriteResult.NoToFolder;
-			Close();
-		}
+    private void bNoToFolder_Click(object sender, EventArgs e)
+    {
+      result = OverwriteResult.NoToFolder;
+      Close();
+    }
 
-		private void bNo_Click(object sender, EventArgs e)
-		{
-			result = OverwriteResult.No;
-			Close();
-		}
+    private void bNo_Click(object sender, EventArgs e)
+    {
+      result = OverwriteResult.No;
+      Close();
+    }
 
     private void bYesToMod_Click(object sender, EventArgs e)
     {
@@ -87,5 +87,12 @@ namespace Fomm.PackageManager
       Properties.Settings.Default.windowPositions.SetWindowPosition("OverwriteForm", this);
       Properties.Settings.Default.Save();
     }
-	}
+
+    private void panel1_Layout(object sender, LayoutEventArgs e)
+    {
+      panel6.Width = panel1.Width / 4;
+      panel5.Width = panel1.Width / 4;
+      panel4.Width = panel1.Width / 4;
+    }
+  }
 }
