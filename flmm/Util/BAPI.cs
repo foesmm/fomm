80eba3711d0a58d532598b1ab812302df23c3d5a
prideslayer <pride3270@gmail.com>
2014-02-17 15:53:25 -0500

    New class to encapsulate BOSS API interface

---
+++ flmm/Util/BAPI.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+/*
+ * BOSS dll importer, refs:
+ * http://msdn.microsoft.com/en-us/library/aa288468%28v=vs.71%29.aspx
+ * http://stackoverflow.com/questions/10852634/using-a-32bit-or-64bit-dll-in-c-sharp-dllimport
+ */
+namespace Fomm.Util
+{
+  public class piBAPI
+  {
+    [DllImport("boss32.dll", EntryPoint = "IsCompatibleVersion", CallingConvention = CallingConvention.Cdecl)]
+    protected static extern bool bapi32_IsCompatibleVersion(UInt32 bossVersionMajor, UInt32 bossVersionMinor, UInt32 bossVersionPatch);
+
+    [DllImport("boss64.dll", EntryPoint = "IsCompatibleVersion", CallingConvention = CallingConvention.Cdecl)]
+    protected static extern bool bapi64_IsCompatibleVersion(UInt32 bossVersionMajor, UInt32 bossVersionMinor, UInt32 bossVersionPatch);
+
+    public piBAPI()
+    {
+      string bossPath;
+      RegistryKey rk;
+
+      // Check registry for BOSS directory and append to path so DllImport can find it.
+      rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\BOSS");
+      if (rk != null)
+      {
+        bossPath = rk.GetValue("Installed Path").ToString();
+        if (bossPath != null)
+        {
+          Environment.SetEnvironmentVariable("Path",
+            Environment.GetEnvironmentVariable("Path") + Path.PathSeparator +
+            bossPath + Path.DirectorySeparatorChar + "API" + Path.DirectorySeparatorChar);
+        }
+      }
+
+      
+    }
+    
+    public bool Is64bitProcess()
+    {
+      return IntPtr.Size == 8;
+    }
+
+    public bool IsCompatibleVersion(UInt32 bossVersionMajor, UInt32 bossVersionMinor, UInt32 bossVersionPatch)
+    {
+      bool ret = false;
+
+      try
+      {
+        ret = Is64bitProcess() ?
+          bapi64_IsCompatibleVersion(bossVersionMajor, bossVersionMinor, bossVersionPatch) :
+          bapi32_IsCompatibleVersion(bossVersionMajor, bossVersionMinor, bossVersionPatch);
+
+        if (!ret)
+        {
+          MessageBox.Show(
+            "BOSS error",
+            "BOSS 2.1.0 or compatible required for this functionality.",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Exclamation);
+        }
+      }
+      catch
+      {
+        MessageBox.Show(
+          "BOSS error",
+          "BOSS not found!",
+          MessageBoxButtons.OK,
+          MessageBoxIcon.Exclamation);
+        ret = false;
+      }
+
+      return ret;
+    }
+  }
+}
