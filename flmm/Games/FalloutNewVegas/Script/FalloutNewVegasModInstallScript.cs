3445847a8ca9ef296a607f54337d7e2d36f7aa1b
prideslayer <pride3270@gmail.com>
2014-03-23 11:17:46 -0400

    untabify

--- flmm/Games/FalloutNewVegas/Script/FalloutNewVegasModInstallScript.cs
+++ flmm/Games/FalloutNewVegas/Script/FalloutNewVegasModInstallScript.cs
@@ -11,95 +11,95 @@ using Fomm.Games.Fallout3.Script;
 
 namespace Fomm.Games.FalloutNewVegas.Script
 {
-	public class FalloutNewVegasModInstallScript : Fallout3ModInstallScript
-	{
-		#region Constructors
+  public class FalloutNewVegasModInstallScript : Fallout3ModInstallScript
+  {
+    #region Constructors
 
-		/// <summary>
-		/// A simple constructor that initializes the object.
-		/// </summary>
-		/// <param name="p_fomodMod">The <see cref="fomod"/> against which to run the script.</param>
-		public FalloutNewVegasModInstallScript(fomod p_fomodMod, ModInstallerBase p_mibInstaller)
-			: base(p_fomodMod, p_mibInstaller)
-		{
-		}
+    /// <summary>
+    /// A simple constructor that initializes the object.
+    /// </summary>
+    /// <param name="p_fomodMod">The <see cref="fomod"/> against which to run the script.</param>
+    public FalloutNewVegasModInstallScript(fomod p_fomodMod, ModInstallerBase p_mibInstaller)
+      : base(p_fomodMod, p_mibInstaller)
+    {
+    }
 
-		#endregion
+    #endregion
 
-		#region Version Checking
+    #region Version Checking
 
-		/// <summary>
-		/// Indicates whether or not NVSE is present.
-		/// </summary>
-		/// <returns><lang cref="true"/> if NVSE is installed; <lang cref="false"/> otherwise.</returns>
-		public override bool ScriptExtenderPresent()
-		{
-			PermissionsManager.CurrentPermissions.Assert();
-			return File.Exists("nvse_loader.exe");
-		}
+    /// <summary>
+    /// Indicates whether or not NVSE is present.
+    /// </summary>
+    /// <returns><lang cref="true"/> if NVSE is installed; <lang cref="false"/> otherwise.</returns>
+    public override bool ScriptExtenderPresent()
+    {
+      PermissionsManager.CurrentPermissions.Assert();
+      return File.Exists("nvse_loader.exe");
+    }
 
-		/// <summary>
-		/// Gets the version of the sript extender that is installed.
-		/// </summary>
-		/// <returns>The version of the sript extender that is installed, or <lang cref="null"/> if no
-		/// sript extender is installed.</returns>
-		public override Version GetScriptExtenderVersion()
-		{
-			PermissionsManager.CurrentPermissions.Assert();
-			if (!File.Exists("nvse_loader.exe"))
-				return null;
-			return new Version(System.Diagnostics.FileVersionInfo.GetVersionInfo("nvse_loader.exe").FileVersion.Replace(", ", "."));
-		}
+    /// <summary>
+    /// Gets the version of the sript extender that is installed.
+    /// </summary>
+    /// <returns>The version of the sript extender that is installed, or <lang cref="null"/> if no
+    /// sript extender is installed.</returns>
+    public override Version GetScriptExtenderVersion()
+    {
+      PermissionsManager.CurrentPermissions.Assert();
+      if (!File.Exists("nvse_loader.exe"))
+        return null;
+      return new Version(System.Diagnostics.FileVersionInfo.GetVersionInfo("nvse_loader.exe").FileVersion.Replace(", ", "."));
+    }
 
-		#endregion
+    #endregion
 
-		#region Ini Management
-		
-		#region Ini Editing
+    #region Ini Management
+    
+    #region Ini Editing
 
-		/// <summary>
-		/// Sets the specified value in the Fallout.ini file to the given value. 
-		/// </summary>
-		/// <param name="p_strSection">The section in the Ini file to edit.</param>
-		/// <param name="p_strKey">The key in the Ini file to edit.</param>
-		/// <param name="p_strValue">The value to which to set the key.</param>
-		/// <param name="p_booSaveOld">Not used.</param>
-		/// <returns><lang cref="true"/> if the value was set; <lang cref="false"/>
-		/// if the user chose not to overwrite the existing value.</returns>
-		public override bool EditFalloutINI(string p_strSection, string p_strKey, string p_strValue, bool p_booSaveOld)
-		{
-			EditINI(((FalloutNewVegasGameMode.SettingsFilesSet)Program.GameMode.SettingsFiles).FODefaultIniPath, p_strSection, p_strKey, p_strValue);
-			return EditINI(((FalloutNewVegasGameMode.SettingsFilesSet)Program.GameMode.SettingsFiles).FOIniPath, p_strSection, p_strKey, p_strValue);
-		}
+    /// <summary>
+    /// Sets the specified value in the Fallout.ini file to the given value. 
+    /// </summary>
+    /// <param name="p_strSection">The section in the Ini file to edit.</param>
+    /// <param name="p_strKey">The key in the Ini file to edit.</param>
+    /// <param name="p_strValue">The value to which to set the key.</param>
+    /// <param name="p_booSaveOld">Not used.</param>
+    /// <returns><lang cref="true"/> if the value was set; <lang cref="false"/>
+    /// if the user chose not to overwrite the existing value.</returns>
+    public override bool EditFalloutINI(string p_strSection, string p_strKey, string p_strValue, bool p_booSaveOld)
+    {
+      EditINI(((FalloutNewVegasGameMode.SettingsFilesSet)Program.GameMode.SettingsFiles).FODefaultIniPath, p_strSection, p_strKey, p_strValue);
+      return EditINI(((FalloutNewVegasGameMode.SettingsFilesSet)Program.GameMode.SettingsFiles).FOIniPath, p_strSection, p_strKey, p_strValue);
+    }
 
-		#endregion
+    #endregion
 
-		#endregion
+    #endregion
 
-		#region Misc Info
+    #region Misc Info
 
-		/// <summary>
-		/// Determines if archive invalidation is active.
-		/// </summary>
-		/// <returns><lang cref="true"/> if archive invalidation is active;
-		/// <lang cref="false"/> otherwise.</returns>
-		public override bool IsAIActive()
-		{
-			return Tools.ArchiveInvalidation.IsActive();
-		}
+    /// <summary>
+    /// Determines if archive invalidation is active.
+    /// </summary>
+    /// <returns><lang cref="true"/> if archive invalidation is active;
+    /// <lang cref="false"/> otherwise.</returns>
+    public override bool IsAIActive()
+    {
+      return Tools.ArchiveInvalidation.IsActive();
+    }
 
-		#endregion
+    #endregion
 
-		#region IDisposable Members
+    #region IDisposable Members
 
-		/// <summary>
-		/// Cleans up used resources.
-		/// </summary>
-		public override void Dispose()
-		{
-			base.Dispose();
-		}
+    /// <summary>
+    /// Cleans up used resources.
+    /// </summary>
+    public override void Dispose()
+    {
+      base.Dispose();
+    }
 
-		#endregion
-	}
+    #endregion
+  }
 }
