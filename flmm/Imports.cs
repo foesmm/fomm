6f66e5ac2d3f913814b4d1bbb64985f0588619e3
Alexander Filippov <a.filippov@technodeep.net>
2015-03-01 11:21:34 +0300

    Remove of ShaderDisasm native calls

--- flmm/Imports.cs
+++ flmm/Imports.cs
@@ -7,52 +7,6 @@ namespace Fomm
 {
   internal static class NativeMethods
   {
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
-    public static extern unsafe sbyte* Disasm(byte[] data, int len, byte Color);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
-    public static extern unsafe byte* Asm(byte[] data, int len);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
-    public static extern unsafe byte* Compile(string data, int len, string EntryPoint, string Profile, byte Debug);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern void ddsInit(IntPtr hwnd);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
-    public static extern IntPtr ddsShrink(byte[] data, int len, out int oSize);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
-    public static extern void ddsClose();
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern IntPtr ddsLoad(byte[] data, int len);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern IntPtr ddsCreate(int width, int height);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern void ddsBlt(IntPtr source, int sL, int sT, int sW, int sH, IntPtr dest, int dL, int dT, int dW,
-                                     int dH);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern IntPtr ddsSave(IntPtr ptr, int format, int mipmaps, out int length);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern void ddsRelease(IntPtr tex);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern void ddsGetSize(IntPtr tex, out int width, out int height);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern IntPtr ddsLock(IntPtr tex, out int length, out int pitch);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern void ddsUnlock(IntPtr tex);
-
-    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
-    public static extern void ddsSetData(IntPtr tex, byte[] data, int len);
-
     [DllImport("kernel32", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
     public static extern int GetPrivateProfileIntA(string section, string value, int def, string path);
 
