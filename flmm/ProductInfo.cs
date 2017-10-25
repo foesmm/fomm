30ee8e034aeb6b42f1b57f56a0cda0c3cfd656a9
Alexander Filippov <a.filippov@technodeep.net>
2017-10-25 23:43:17 +0300

    Made app GUID match Installer GUID

--- flmm/ProductInfo.cs
+++ flmm/ProductInfo.cs
@@ -7,7 +7,7 @@ namespace Fomm
   /// </summary>
   public static class ProductInfo
   {
-    public static readonly Guid GUID = new Guid("F97B1D1B-F722-4F37-902E-9FB501FADD0E");
+    public static readonly Guid GUID = new Guid("072C2AEF-16B2-46B7-BA7F-D0CAA7B4F89F");
     public const string ShortName = "FOMM";
     public const string FullName = "Fallout Mod Manager";
     public const string Version = "0.14.13";
