8ec684118f97d1cec68c0cadae70d6b568e5a3d6
Alexander Filippov <a.filippov@technodeep.net>
2018-01-15 22:13:45 +0300

    Batch shader processor

---
+++ BatchShaderProcessor/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace BatchShaderProcessor
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+      this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+      this.button1.Location = new System.Drawing.Point(0, 0);
+      this.button1.Margin = new System.Windows.Forms.Padding(8);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(284, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Unpack Shaders";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.button2.Enabled = false;
+      this.button2.Location = new System.Drawing.Point(0, 23);
+      this.button2.Margin = new System.Windows.Forms.Padding(8);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(284, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Pack Shaders";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.ClientSize = new System.Drawing.Size(284, 46);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "Form1";
+      this.ShowIcon = false;
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Batch Shader Processor";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+  }
+}
+
---
+++ BatchShaderProcessor/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatchShaderProcessor
+{
+  public partial class Form1 : Form
+  {
+    private class Shader
+    {
+      internal string name;
+      internal char[] name2;
+      internal byte[] data;
+    }
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
+    public static extern unsafe sbyte* Disasm(byte[] data, int len, byte Color);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
+    public static extern unsafe byte* Asm(byte[] data, int len);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
+    public static extern unsafe byte* Compile(string data, int len, string EntryPoint, string Profile, byte Debug);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern void ddsInit(IntPtr hwnd);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
+    public static extern IntPtr ddsShrink(byte[] data, int len, out int oSize);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi)]
+    public static extern void ddsClose();
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern IntPtr ddsLoad(byte[] data, int len);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern IntPtr ddsCreate(int width, int height);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern void ddsBlt(IntPtr source, int sL, int sT, int sW, int sH, IntPtr dest, int dL, int dT, int dW,
+                                     int dH);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern IntPtr ddsSave(IntPtr ptr, int format, int mipmaps, out int length);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern void ddsRelease(IntPtr tex);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern void ddsGetSize(IntPtr tex, out int width, out int height);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern IntPtr ddsLock(IntPtr tex, out int length, out int pitch);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern void ddsUnlock(IntPtr tex);
+
+    [DllImport("ShaderDisasm", CharSet = CharSet.Ansi), SuppressUnmanagedCodeSecurity]
+    public static extern void ddsSetData(IntPtr tex, byte[] data, int len);
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      openFileDialog1.Filter = "Fallout 3 shader package (*.sdp)|*.sdp";
+      openFileDialog1.Title = "Select Shader package to edit";
+      if (openFileDialog1.ShowDialog() != DialogResult.OK || openFileDialog1.FileName == null)
+      {
+        return;
+      }
+      Open(openFileDialog1.FileName);
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private unsafe void Open(string filename)
+    {
+      if (folderBrowserDialog1.ShowDialog() != DialogResult.OK)
+      {
+        return;
+      }
+      var path = Path.GetFullPath(filename);
+      var br = new BinaryReader(File.OpenRead(path), Encoding.Default);
+      var unknown = br.ReadUInt32();
+      var num = br.ReadInt32();
+      br.ReadInt32();
+      for (var i = 0; i < num; i++)
+      {
+        var s = new Shader();
+        var name = br.ReadChars(0x100);
+        s.name = "";
+        s.name2 = name;
+        for (var i2 = 0; i2 < 100; i2++)
+        {
+          if (name[i2] == '\0')
+          {
+            break;
+          }
+          s.name += name[i2];
+        }
+        var size = br.ReadInt32();
+        s.data = br.ReadBytes(size);
+
+        var ptr = Disasm(s.data, s.data.Length, 0);
+        var text = new string(ptr);
+        text = text.Replace("" + (char)10, Environment.NewLine);
+
+        var outfile = Path.Combine(folderBrowserDialog1.SelectedPath, s.name);
+        File.WriteAllText(outfile, text);
+      }
+    }
+  }
+}
